40390bcf2779b8cc69c02ece3f44db0d004da594
zdjzce923 <493904954@qq.com>
2022-02-11 08:59:55 +0800

    原型

--- 8.对象、类、面向对象编程/理解原型.js
+++ 8.对象、类、面向对象编程/理解原型.js
@@ -45,6 +45,24 @@ let biped = {
 	numLegs: 2 
 }
 
-let person3 = Object.create(biped)
+let person3 = Object.create( biped )
 person3.name = 'zdj'
 console.log( biped.numLegs, person3 )
+
+/* 在通过对象访问属性时，会按照这个属性的名称开始搜索。搜索开始于对象实例本身。如果在这个
+实例上发现了给定的名称，则返回该名称对应的值。如果没有找到这个属性，则搜索会沿着指针进入原
+型对象，然后在原型对象上找到属性后，再返回对应的值。因此，在调用person1.sayName()时，会
+发生两步搜索。首先，JavaScript 引擎会问：“person1 实例有sayName 属性吗？”答案是没有。然后，
+继续搜索并问：“person1 的原型有 sayName 属性吗？”答案是有。于是就返回了保存在原型上的这
+个函数。在调用person2.sayName()时，会发生同样的搜索过程，而且也会返回相同的结果。这就是
+原型用于在多个对象实例间共享属性和方法的原理。 */
+/* 虽然可以查看原型对象的属性，但是不可以修改，如果在实例上添加与原型对象同样的属性名，那么这个属性会遮住原型对象上的属性 */
+person3.numLegs = '325435345'
+console.log( person3, person3.numLegs)
+// 无论是原型上或是实例上的属性，只要一方存在，使用 in 操作符就会返回 true，可以通过对象访问且可以被枚举的属性都会返回 true
+console.log('person3:name :', 'name' in person3) // true
+// 如果想知道属性是否存在于原型上，可以将 hasOwnProperty() 和 in 结合使用
+function protoHasAttribute( object, attr ) {
+	return !object.hasOwnProperty( attr ) && (attr in object)
+}
+console.log('protoHasAttribute( person3,name ):', protoHasAttribute( person3, 'name' )) // false
