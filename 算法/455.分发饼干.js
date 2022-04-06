1651d831a32d5e6dc8cd6133c5b5989685be9375
zdjzce923 <493904954@qq.com>
2022-04-06 10:59:46 +0800

    update

--- 算法/455.分发饼干.js
+++ 算法/455.分发饼干.js
@@ -9,9 +9,24 @@
  * @param {number[]} g
  * @param {number[]} s
  * @return {number}
+ * 深度优先 将两个数组排序 优先满足最后一个孩子 
+ * [1,2] [1,2,3]
  */
-var findContentChildren = function(g, s) {
 
+const findContentChildren = function(g, s) {
+	g.sort((a, b) => a - b)
+	s.sort((a, b) => a - b)
+	let childLen = g.length, biscuitLen = s.length
+	let count = 0
+	for(let i = 0, j = 0; i < childLen && j < biscuitLen; i++, j++) {
+		while (j < biscuitLen && g[i] > s[j]) {
+			j++;
+		}
+		if (j < biscuitLen) {
+				count++;
+		}
+	}
+	return count;
 };
 // @lc code=end
 
