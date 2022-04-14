c381c014f3d649c25f254da5ba4ed1a454fa7159
zdjzce923 <493904954@qq.com>
2022-04-14 11:21:30 +0800

    移除元素:双指针

--- 算法/27.移除元素.js
+++ 算法/27.移除元素.js
@@ -13,7 +13,7 @@
  */
 
 /* 简单解法 */
-var removeElement = function (nums, val) {
+/* var removeElement = function (nums, val) {
 	for (let i = 0; i < nums.length; i++) {
 		if (nums[i] === val) {
 			nums.splice(i, 1)
@@ -21,8 +21,24 @@ var removeElement = function (nums, val) {
 		}
 	}
 	return nums.length
-};
+}; */
 
+/**
+ * 双指针不优化版本
+ * 将左边的指针置0 再设置右指针 当右指针值不等于value时 就改变nums[left]的值为 nums[right] 并且让 left 增加，如果右指针的值是val 不能输出到数组里 left 不动
+ * 缺点： 如果开头就有要删除的元素 就意味着后面的每个数字都要向前移一位
+ */
+
+var removeElement = function (nums, val) {
+	let left = 0
+	for (let right = 0; right < nums.length; right++) {
+		if(nums[right] != val) {
+			nums[left] = nums[right]
+			left++
+		}
+	}
+	return left
+};
 
 /*  */
 // @lc code=end
