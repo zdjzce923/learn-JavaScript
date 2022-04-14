cf7d0c781ce8f6ec2efa6dd46f7b8bb8ff111fbf
zdjzce923 <493904954@qq.com>
2022-04-14 11:45:44 +0800

    移除元素:双指针优化

--- 算法/27.移除元素.js
+++ 算法/27.移除元素.js
@@ -25,11 +25,13 @@
 
 /**
  * 双指针不优化版本
- * 将左边的指针置0 再设置右指针 当右指针值不等于value时 就改变nums[left]的值为 nums[right] 并且让 left 增加，如果右指针的值是val 不能输出到数组里 left 不动
+ * 将左边的指针置 0 再设置右指针 当右指针值不等于 value 时
+ * 就改变 nums[left] 的值为 nums[right] 并且让 left 增加，
+ * 如果右指针的值是 val 不能输出到数组里 left 不动
  * 缺点： 如果开头就有要删除的元素 就意味着后面的每个数字都要向前移一位
  */
 
-var removeElement = function (nums, val) {
+/* var removeElement = function (nums, val) {
 	let left = 0
 	for (let right = 0; right < nums.length; right++) {
 		if(nums[right] != val) {
@@ -38,8 +40,24 @@ var removeElement = function (nums, val) {
 		}
 	}
 	return left
-};
+}; */
 
-/*  */
+/**
+ * 双指针优化版本 让右指针从最右边开始 
+ * 判断left对应的值是否跟val相等 
+ * 相等则将最右边的值替换到left上 接着让right-- left++
+ */
+const removeElement = function (nums, val) {
+	let left = 0, right = nums.length
+	while (left < right) {
+		if (nums[left] === val) {
+			nums[left] = nums[right - 1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
 // @lc code=end
 
