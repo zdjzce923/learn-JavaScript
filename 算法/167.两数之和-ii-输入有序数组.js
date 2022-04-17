d2010a931361e294e09fe5ed3380ffedde2712dc
zdjzce923 <493904954@qq.com>
2022-04-17 18:29:46 +0800

    两数之和输入有序数组:双指针

--- 算法/167.两数之和-ii-输入有序数组.js
+++ 算法/167.两数之和-ii-输入有序数组.js
@@ -9,9 +9,22 @@
  * @param {number[]} numbers
  * @param {number} target
  * @return {number[]}
+ * 双指针 设置左右指针 查看大小是否等于目标值 在依次判断比目标值大和小的情况
+ * [2,7,11,15]
  */
-var twoSum = function(numbers, target) {
+var twoSum = function (numbers, target) {
+  let left = 0, right = numbers.length - 1, numbersLen = numbers.length
+  while (left <= right) {
+    let sum = numbers[left] + numbers[right]
+    if (sum === target) {
+      return [left + 1, right + 1]
+    } else if (sum > target) {
+      right--
+    } else {
+      left++
+    }
 
+  }
 };
 // @lc code=end
 
