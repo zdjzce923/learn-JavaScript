331fd2a4d60deb8e829626fa67ae9f879d0391a9
zdjzce923 <493904954@qq.com>
2022-04-17 19:31:00 +0800

    反转字符串:暴力解 双指针

--- 算法/344.反转字符串.js
+++ 算法/344.反转字符串.js
@@ -9,8 +9,29 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
+/* var reverseString = function (s) {
+  let left = 0
+  let right = s.length - 1
+  while (left < right) {
+    let temp = s[left]
+    s[left] = s[right]
+    s[right] = temp
+    left++
+    right--
+  }
+}; */
 
-};
+// 暴力解
+var reverseString = function (s) {
+  let rightLen = s.length - 1
+  for (let i = 0; i < s.length; i++) {
+    if (i >= rightLen) { break }
+    let temp = s[i]
+    s[i] = s[rightLen]
+    s[rightLen] = temp
+    rightLen--
+  }
+}
+
 // @lc code=end
 
