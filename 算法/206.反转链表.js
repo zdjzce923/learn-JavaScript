5ed4b052384cfaa443538ec00200bb147035ff4b
zdjzce923 <493904954@qq.com>
2022-04-18 15:56:54 +0800

    反转链表:递归 妙啊

--- 算法/206.反转链表.js
+++ 算法/206.反转链表.js
@@ -20,10 +20,10 @@
  */
 
 /**
- * 思路: 设置左右指针将节点的指向反过来 首先创建节点保存状态值(即要改变指向的节点)为null 当前的节点如果不为null 就接着遍历 
+ * 思路: 设置左右指针将节点的指向反过来 首先创建节点保存状态值(即要改变指向的节点)为null 当前的节点如果不为null 就接着遍历
  * 先保存下个节点 再将当前节点指向状态值 最后将要遍历的节点值换为原来的 next 节点
  */
-var reverseList = function (head) {
+/* var reverseList = function (head) {
   let prev = null;  // 左指针
   let curr = head;  // 右指针
   while (curr) {
@@ -33,6 +33,26 @@ var reverseList = function (head) {
     curr = next
   }
   return prev;
+} */
+
+/**
+ * 递归解法
+ * 退出条件：head的next为null时 return head
+ * 返回值：递完的头结点
+ * 输入：1 → 2 → 3 → 4 → 5 
+ *                      5.next === null return 5
+ *          4.next.next = 4 4.next = null
+ * 输出：5 → 4 → 3 → 2 → 1
+ */
+
+var reverseList = function (head) {
+  if (head === null || head.next === null) {
+    return head
+  }
+  let cur = reverseList(head.next)  // cur就是递完的头结点最后返回这个头结点
+  head.next.next = head    // 结合上面的例子 就是 4.next.next = 4 就是 5.next = 4 
+  head.next = null         // 换完了指向把当前节点的指向置为 null
+  return cur
 }
 // @lc code=end
 
