1e937d840664d021b86ba2709a3d8750cbac72e7
zdjzce923 <493904954@qq.com>
2022-04-19 18:28:04 +0800

    update

---
+++ 算法/160.相交链表.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=javascript
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ * 1 -> 2 -> 3 -> 4 -> 5  m
+ * 4 -> 3 -> 4 -> 5       n
+ * 
+ */
+
+/**
+ * 
+ */
+const getIntersectionNode = function (headA, headB) {
+  while (headA) {
+
+  }
+  
+}
+// @lc code=end
+
