8e07f2d780af06212fb460d0cf88e78d56b49574
zdjzce923 <493904954@qq.com>
2022-04-23 17:04:47 +0800

    环形链表:双指针/对象

--- 算法/142.环形链表-ii.js
+++ 算法/142.环形链表-ii.js
@@ -20,8 +20,44 @@
  *      ↑---------|
  */
 
-var detectCycle = function(head) {
-    
-};
+/**
+ * 方法1 用对象去保存 每次都放进去 如果对象中有这个节点就返回该节点
+ */
+/* var detectCycle = function (head) {
+  let nodeSet = new Set()
+  while (head !== null) {
+    if (nodeSet.has(head)) {
+      return head
+    }
+    nodeSet.add(head)
+    head = head.next
+  }
+  return null
+} */
+
+/**
+ * 方法2 用快慢指针 如果他们相遇了 就说明是环形链表 再进行判断拿到
+ */
+var detectCycle = function (head) {
+  if (head === null) { return null }
+  let slow = fast = head
+  while (fast !== null) {
+    slow = slow.next
+    if (fast.next !== null) {
+      fast = fast.next.next
+    } else {
+      return null
+    }
+    if (fast === slow) {
+      let ptr = head
+      while (ptr !== slow) {
+        ptr = ptr.next
+        slow = slow.next
+      }
+      return ptr
+    }
+  }
+  return null
+}
 // @lc code=end
 
