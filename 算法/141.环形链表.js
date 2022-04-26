ba3d0c42f277e4f19a39545da56c0312e9fa4075
zdjzce923 <493904954@qq.com>
2022-04-26 11:03:56 +0800

    二叉树前序遍历 栈 循环

--- 算法/141.环形链表.js
+++ 算法/141.环形链表.js
@@ -17,31 +17,47 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-const hasCycle = function(head) {
-	/* //  1. 暴力解法 每次循环如果Set对象里没有包含已经有的next节点，则添加，否则return true。没有下个节点则return false
-	let cache = new Set()
+/* const hasCycle = function (head) {
+  //  1. 暴力解法 每次循环如果Set对象里没有包含已经有的next节点，则添加，否则return true。没有下个节点则return false
+  let cache = new Set()
+  
+  while(head) {
+    if(cache.has(head)) { 
+      return true
+    } else {
+      cache.add(head)
+    }
+    head = head.next
+  }
+  return false
+ 
+}; */
 
-	while(head) {
-		if(cache.has(head)) { 
-			return true
-		} else {
-			cache.add(head)
-		}
-		head = head.next
-	}
-	return false
-	*/
-	//  2. 双指针 拿两个指针去跑 快的指针肯定会套圈遇上慢的指针
-	let fast = head
-	let slow = head
-	while(fast && fast.next) {
-		fast = fast.next.next
-		slow = slow.next
-		if( fast === slow ) {
-			return true
-		}
-	}
-	return false
-};
+// 2. 双指针 拿两个指针去跑 快的指针肯定会套圈遇上慢的指针
+/* const hasCycle = function (head) {
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+    if (fast === slow) {
+      return true
+    }
+  }
+  return false
+}
+ */
+
+// 1 -> 2 -> 3 -> 4
+//      ↑---------
+var hasCycle = function (head) {
+  if (head == null || head.next == null)
+    return false;
+  if (head.next == head)
+    return true;
+  let headNext = head.next;
+  head.next = head;
+  return hasCycle(headNext);
+}
 // @lc code=end
 
--- 算法/144.二叉树的前序遍历.js
+++ 算法/144.二叉树的前序遍历.js
@@ -17,19 +17,42 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-const preorderTraversal = function (root) {
-	const arr = []
 
-	function leftTraverse (root) {
-		if (root === null) {
-			return
-		}
-		arr.push(root.val)
-		leftTraverse(root.left)
-		leftTraverse(root.right)
-	}
-	leftTraverse(root)
-	return arr
-};
+/**
+ * 二叉树的前序遍历 循环 栈解法 
+ * 前序遍历遵照 根节点 左节点 右节点的遍历方式 将根节点放入栈中 接着将节点弹出栈 后续遍历左右节点
+ */
+/* const preorderTraversal = function (root) {
+  const arr = []
+
+  function leftTraverse (root) {
+    if (root === null) {
+      return
+    }
+    arr.push(root.val)
+    leftTraverse(root.left)
+    leftTraverse(root.right)
+  }
+  leftTraverse(root)
+  return arr
+}; */
+const preorderTraversal = function (root) {
+  if (root === null) {
+    return []
+  }
+
+  let res = []
+  let cacheNode = [root]
+
+  while (cacheNode.length) {
+    const cur = cacheNode.pop() // 弹出
+    res.push(cur.val)
+    cur.right && cacheNode.push(cur.right)
+    cur.left && cacheNode.push(cur.left)
+  }
+
+  return res
+
+}
 // @lc code=end
 
