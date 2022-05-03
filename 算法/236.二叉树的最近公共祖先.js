4f3e5907b3090783c3e612a42e932e67a919bcc8
zdjzce923 <493904954@qq.com>
2022-05-03 13:22:11 +0800

    二叉树的最近公共祖先: 递归

--- 算法/236.二叉树的最近公共祖先.js
+++ 算法/236.二叉树的最近公共祖先.js
@@ -21,23 +21,20 @@
 // 先找 两个节点的祖先 如果不同接着往上找
 var lowestCommonAncestor = function (root, p, q) {
   // 如果root为空或者为p q 则返回 因为找到了一个节点就不能接着往下找了 不可能比这更深
+  // 递归的核心在于 到最深的节点不能再递的时候 也就是为空或者找到一个目标节点时 就要返回这个节点
+  // 如果左子树啥也没找着 那么两个节点一定在右子树上
   if (root === null || root === p || root === q) {
     return root
   }
-
   const left = lowestCommonAncestor(root.left, p, q)
   const right = lowestCommonAncestor(root.right, p, q)
 
-  if (left === null && right === null) {
-    return null
+  if (!left) {
+    return right
   }
 
   if (!right) {
     return left
-  }
-
-  if (!left) {
-    return right
   }
 
   return root
