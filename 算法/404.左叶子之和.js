1a0cca83b959c01c57b95d1f03fbd47fd14a2686
zdjzce923 <493904954@qq.com>
2022-06-02 16:34:39 +0800

    左叶子之和 递归

--- 算法/404.左叶子之和.js
+++ 算法/404.左叶子之和.js
@@ -21,8 +21,10 @@
 /* 
  * 需要找到所有左叶子节点并全部累加 意味着需要把左节点跟右节点都放到队列里 再把左节点单独维护一个队列
  * 进行判断 如果左节点队列中有当前弹出的这个节点并且有左右节点 就直接弹出 如果此时左节点列表中还有这个节点说明它是叶子结点 直接累加
+ * 其实可以到左节点的时候就查看他是否是叶子结点。。。
  */
-var sumOfLeftLeaves = function (root) {
+
+/* var sumOfLeftLeaves = function (root) {
     if (root === null) return
     let queue = [root]
     let leftQueue = []
@@ -46,6 +48,30 @@ var sumOfLeftLeaves = function (root) {
     }
     return sum
 
-};
+}; */
+
+
+// 深度优先
+// 查看左节点是否是叶子结点 如果是将值进行累加 不是则接着递归
+var sumOfLeftLeaves = function (root) {
+    if (root === null) return 0
+    return dfs(root)
+}
+const dfs = (root) => {
+    let sum = 0
+    if (root.left) {
+        if (!root.left.right && !root.left.left) {
+            sum += root.left.val
+        } else {
+            sum += dfs(root.left)
+        }
+    }
+
+    if (root.right) {
+        sum += dfs(root.right)
+    }
+    return sum
+}
+
 // @lc code=end
 
